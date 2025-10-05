287135b589239055ea643d1d25d31eed269ee58d
abhishek-biyani <coderabhishek08@gmail.com>
2025-10-05 20:27:37 +0530

    updated gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   base: "/Informed-health/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
